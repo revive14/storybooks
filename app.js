bd34f52b04814154a8a40c5bc4a9799a09a243e3
Santa <has.hamza247@gmail.com>
2017-10-14 18:53:36 -0500

    handled auth

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
+const auth = require('./routes/auth');
+
+//Passport Config
+require('./config/passport')(passport);
+
+
+
 
 const port = process.env.PORT || 5000;
 
@@ -10,7 +18,8 @@ app.get('/',(req,res)=>{
 })
 
 
-
+//USE Routes
+app.use('/auth', auth);
 
 app.listen(port,()=>{
     console.log(`server started at port ${port} `)
